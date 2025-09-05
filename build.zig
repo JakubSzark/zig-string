d41dd899a9c557cd24e363c4c5c491590e97c94c
Ryoga-exe <git@ryoga.dev>
2025-09-05 14:13:35 +0900

    feat: update build.zig

--- build.zig
+++ build.zig
@@ -4,14 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("string", .{ .root_source_file = b.path("zig-string.zig") });
+    _ = b.addModule("string", .{
+        .root_source_file = b.path("zig-string.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-    var main_tests = b.addTest(.{
+    const test_mod = b.addModule("string-tests", .{
         .root_source_file = b.path("zig-string-tests.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    const main_tests = b.addTest(.{
+        .root_module = test_mod,
+    });
+
+    const run_main_tests = b.addRunArtifact(main_tests);
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 }
